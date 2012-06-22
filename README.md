cd6c9e00a152ff276fb695c836e6488bfa3d8053
Jordi Llonch <llonchj@atcubic.com>
2012-06-22 15:03:19 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-yourls_plugins
+Yourls Plugins
 ==============
 
 Yourls Plugins (www.yourls.org)
+
+This is my personal collection of Yourls Plugins.
+
+- qs_forward - Pushes the Short URL Query String into the Destination URL
+    (requires a Yourls 'greater than' 1.5.4)
+
+Thanks to Ozh Richard and Lester Chan for Yourls.
---
+++ qs_forward/plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Query String Forward
+Plugin URI: https://github.com/llonchj/yourls_plugins
+Description: Pushes the Short URL Query String into the Destination URL
+Version: 1.0
+Author: Jordi Llonch
+Author URI: https://github.com/llonchj
+*/
+
+// Hook our custom function into the 'pre_redirect' event
+yourls_add_filter('redirect', 'qs_forward_redirect' );
+
+// Our custom function that will be triggered when the event occurs
+function qs_forward_redirect($url, $code) {
+    $parsed_url = parse_url($url);
+
+    parse_str($_SERVER['QUERY_STRING'], $query);
+    parse_str($parsed_url["query"], $url_query);
+    
+    $a = array_merge($query, $url_query);
+    $parsed_url["query"] = http_build_query($a);
+    $new_url = $parsed_url["scheme"]."://".$parsed_url["host"].$parsed_url["path"];
+    if (is_set($parsed_url["query"]))
+        $new_url += "?".$parsed_url["query"];
+
+    return $new_url;
+}
+
+?>
