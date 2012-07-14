c3ae5d634fe128c24e973b3e150b28f6537ff278
Jordi Llonch <llonchj@atcubic.com>
2012-07-15 08:01:22 +1000

    clean ? when not needed. fix by XL-Network.

--- qs_forward/plugin.php
+++ qs_forward/plugin.php
@@ -21,7 +21,7 @@ function qs_forward_redirect($url, $code) {
     $a = array_merge($query, $url_query);
     $parsed_url["query"] = http_build_query($a);
     $new_url = $parsed_url["scheme"]."://".$parsed_url["host"].$parsed_url["path"];
-    if (isset($parsed_url["query"])) 
+    if (isset($parsed_url["query"]) && /* fix by XL-Network. Thank you!*/$parsed_url["query"] != "")
         $new_url = "$new_url?".$parsed_url["query"];
     return $new_url;
 }
